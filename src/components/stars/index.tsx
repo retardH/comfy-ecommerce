be38d0c22868221134d06db4f18d04cffe2ae771
htetzarni <loowmaad@gmail.com>
2023-11-20 17:08:14 +0630

    initialize components

---
+++ src/components/stars/index.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+const Stars = () => {
+  return <Wrapper>Starts</Wrapper>;
+};
+
+export default Stars;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  span {
+    color: #ffb900;
+    font-size: 1rem;
+    margin-right: 0.25rem;
+  }
+  p {
+    margin-left: 0.5rem;
+    margin-bottom: 0;
+  }
+  margin-bottom: 0.5rem;
+`;
