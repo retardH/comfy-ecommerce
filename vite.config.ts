474aff04f73141883dbc7efeaf0a38be4d9116a8
Htet Zarni <htet.zarni@kbzbank.com>
2025-01-21 15:46:34 +0630

    fix: backend endpoint url change

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,14 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-// import path from 'path';
-
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   mode,
-//   resolve: {
-//     alias: {
-//       '@': path.join(__dirname, 'src'),
-//     },
-//   },
-//   plugins: [react()],
-// }) => {
-//   return {
-
-//   }
-// };
+import path from 'path';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
+    mode,
+    resolve: {
+      alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    },
     define: {
       'process.env.AUTH_DOMAIN': JSON.stringify(env.AUTH_DOMAIN),
       'process.env.AUTH_CLIENT_ID': JSON.stringify(env.AUTH_CLIENT_ID),
